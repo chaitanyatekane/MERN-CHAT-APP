4991a3f9093e9bbcd48bdaac84e1f41e9d8ec8d4
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-11-13 18:04:03 +0530

    install nodemon

--- backend/server.js
+++ backend/server.js
@@ -21,4 +21,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log(`Server Started On PORT ${PORT}`));
+app.listen(5000, console.log(`Server Started On PORT ${PORT}!`));
