ed72dea929321ce5b3d929cbbd2c6f7bb6fb882c
Chaitanya Santosh Tekane <chaitanyatekne5@gmail.com>
2021-11-13 17:57:47 +0530

    create APIs with express-js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const chats = require("./data/data");
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API Is Running");
+});
+
+app.get("api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
 app.listen(5000, console.log("Server Started On PORT 5000"));
